34e778bfc4b4de7f3916ce5aa5b38f4bf9fde921
Numan Safi <numansafi@live.com>
2022-06-14 20:14:12 +0100

    added axios through cdn

---
+++ src/index.js
@@ -0,0 +1,7 @@
+axios.defaults.headers.common["Authorization"] =
+  "cc5937bc-590b-4ae7-a95f-dd70cf670735";
+axios.defaults.baseURL = "https://api.thecatapi.com/v1/";
+
+const config = {
+  headers: {},
+};
