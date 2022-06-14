37fc7606ce9ca1f1b0c84edc7110f956a90ae773
Numan Safi <numansafi@live.com>
2022-06-14 20:59:52 +0100

    used axios to make a get request to catAPI

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-axios.defaults.headers.common["Authorization"] =
+axios.defaults.headers.common["x-api-key"] =
   "cc5937bc-590b-4ae7-a95f-dd70cf670735";
 axios.defaults.baseURL = "https://api.thecatapi.com/v1/";
 
-const config = {
-  headers: {},
+const getCatGif = async () => {
+  try {
+    const response = await axios.get(
+      `images/search?limit=1&size=full&mime_types=gif`
+    );
+    const data = response.data[0];
+
+    console.log(response);
+    showOutput(data);
+  } catch (errors) {
+    console.error(errors);
+  }
 };
+
+function showOutput(res) {
+  document.getElementById("res").innerHTML = `
+  <div class="card card-body mb-3">
+  <img src="${res.url}" alt="">
+  </div>
+`;
+}
+
+document.getElementById("catgifs").addEventListener("click", getCatGif);
