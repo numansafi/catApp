c137a969d8a444d59931de6afea4c639115e29c0
Numan Safi <numansafi@live.com>
2022-06-15 14:25:04 +0100

    rendering data from breed info

--- src/index.js
+++ src/index.js
@@ -5,32 +5,64 @@ axios.defaults.baseURL = "https://api.thecatapi.com/v1/";
 
 const catGifPath = "/images/search?mime_types=gif";
 const catPicPath = "/images/search?mime_types=png,jpg";
-const breedSearchValue = "beng";
-console.log(breedSearchValue);
-const searchByBreedPath = `/images/search?breed_ids=${breedSearchValue}`;
+
+const searchByBreedPath = `/images/search?breed_ids=amis`;
 
 function getCatGif() {
   axios
     .get(catGifPath)
-    .then((res) => showOutput(res))
+    .then((res) => displayImage(res, "Cat Gif"))
     .catch((err) => console.error(err));
 }
 function getCatPic() {
   axios
     .get(catPicPath)
-    .then((res) => showOutput(res))
+    .then((res) => displayImage(res, "Cat Pic"))
     .catch((err) => console.error(err));
 }
 function searchByBreed() {
   axios
     .get(searchByBreedPath)
-    .then((res) => showOutput(res))
+    .then((res) => {
+      console.log(res);
+      console.log(res.data[0].breeds[0].name);
+      displayBreedInfo(res);
+    })
     .catch((err) => console.error(err));
 }
 
-function showOutput(res) {
+function displayImage(res, header) {
   const imageUrl = res.data[0].url;
-  return (document.getElementById("cat_image").src = imageUrl);
+  document.getElementById("cat_header").innerHTML = header;
+  document.getElementById("render_res").innerHTML = `
+  <img class="thumbnail img-responsive" src="${imageUrl}" />`;
+}
+
+function displayBreedInfo(res) {
+  const data = res.data[0];
+  const breedInfo = res.data[0].breeds[0];
+  document.getElementById("cat_header").innerHTML = breedInfo.name;
+  document.getElementById("render_res").innerHTML = `
+  <div class="row">
+    <div class="col">
+    <img class="card-img-top" src="${data.url}">
+    </div>
+    <div class="col">
+      <div class="card-body">
+        <h5 class="card-title">${breedInfo.name}</h5>
+          <p class="card-text">${breedInfo.description}</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">${breedInfo.temperament}</li>
+        <li class="list-group-item">Life Span: ${breedInfo.life_span} Years</li>
+        <li class="list-group-item">Origin: ${breedInfo.origin}</li>
+      </ul>
+      <div class="card-body">
+        <a href="${breedInfo.wikipedia_url}" class="card-link">Wikipedia</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+  </div>
+`;
 }
 
 document.getElementById("cat_gif").addEventListener("click", getCatGif);
